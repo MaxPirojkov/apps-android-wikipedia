07a93be0b1386543897dc4b205915e580a0e345c
maxim.pirozhkov <maxim.pirozhkov@exness.com>
2026-06-24 08:52:08 +0300

    dockerfile marothonfile

--- app/src/androidTest/java/org/wikipedia/works/lesson29/AuthorizeTest.kt
+++ app/src/androidTest/java/org/wikipedia/works/lesson29/AuthorizeTest.kt
@@ -1,6 +1,8 @@
 package org.wikipedia.works.lesson29
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.kaspersky.components.alluresupport.withForcedAllureSupport
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import io.qameta.allure.kotlin.AllureId
 import org.junit.Rule
@@ -9,12 +11,13 @@ import org.wikipedia.main.MainActivity
 import org.wikipedia.works.lesson07.OnboardingScreen
 import org.wikipedia.works.lesson20.HW.namedSteps
 
-class AuthorizeTest: TestCase() {
+class AuthorizeTest: TestCase( kaspressoBuilder = Kaspresso.Builder.withForcedAllureSupport()) {
 
     @get:Rule
     val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
+    @SmokeTest
     @AllureId("1")
     fun authorize() {
         run {
---
+++ app/src/androidTest/java/org/wikipedia/works/lesson29/SmokeTest.kt
@@ -0,0 +1,7 @@
+package org.wikipedia.works.lesson29
+
+import org.maplibre.android.plugins.annotation.Annotation
+
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.FUNCTION)
+annotation class SmokeTest()
